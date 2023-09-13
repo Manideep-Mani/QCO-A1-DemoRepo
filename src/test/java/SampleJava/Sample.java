80d2615438220d0ea8debcc84d28d0028b3c0791
mamid <mamid@LAPTOP-4CCF5FDA>
2023-09-13 16:02:21 +0530

    Tc002

--- src/test/java/SampleJava/Sample.java
+++ src/test/java/SampleJava/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("Hi this is my first commit");
 	System.out.println("Hello");
+	System.out.println("Refer");
 }
 }
